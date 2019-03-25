3cea6dad1e203430ed67f3b84c6af24997cd0f6a
synergy2411 <synergy2411@outlook.com>
2019-03-25 17:40:39 +0530

    app commited

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector : "app-users",
+    templateUrl : `./users.component.html`
+})
+
+export class UsersComponent{
+    user = {
+        firstName : "Bill",
+        lastName : "Gates",
+        dob : new Date("Dec 24 '1965"),
+        isWorking : true,
+        income : 50000,
+        company : "Microsoft Inc.",
+        image : "./assets/images/bill.jpg"
+    }
+
+    moreInfo(user : any){
+        alert(`${user.firstName} is working with ${user.company}!!`);
+    }
+
+}
